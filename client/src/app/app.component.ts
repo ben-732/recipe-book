669ed6cb430d9f9aad6fcf8e1d3031189ea9d83e
Ben <22252563+ben-732@users.noreply.github.com>
2024-02-18 23:22:21 +1300

    Generic styles

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Recipe Book';
