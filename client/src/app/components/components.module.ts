00429190a000071a0c7bc51594236f55e6071d20
Ben <22252563+ben-732@users.noreply.github.com>
2024-04-11 23:49:49 +1200

    Displaying tags on recipe cards

--- client/src/app/components/components.module.ts
+++ client/src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './generic/button/button.component';
 import { RouterModule } from '@angular/router';
+import { TagComponent } from './generic/tag/tag.component';
 
 @NgModule({
-  declarations: [ButtonComponent],
+  declarations: [ButtonComponent, TagComponent],
   imports: [CommonModule, RouterModule],
-  exports: [ButtonComponent],
+  exports: [ButtonComponent, TagComponent],
 })
 export class ComponentsModule {}
---
+++ client/src/app/components/generic/tag/tag.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { TagSummary } from '../../../../generated/models';
+// import {TagSummary} from '../../../generated/models';
+@Component({
+  selector: 'recipe-tag',
+  templateUrl: './tag.component.html',
+  styleUrl: './tag.component.scss',
+})
+export class TagComponent {
+  @Input() tag!: TagSummary;
+}
